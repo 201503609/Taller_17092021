3615442000ed08e52317557f888334ab09aa0665
Diego Berrios <201503609@ingenieria.usac.edu.gt>
2021-09-17 18:31:39 -0600

    ADD: CRUD Vb
    Se agrega ejemplo de CRUD

---
+++ Taller_CRUD/Form1.Designer.vb
@@ -0,0 +1,234 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvEstudiantes = New System.Windows.Forms.DataGridView()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.txtEdad = New System.Windows.Forms.TextBox()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.txtTel = New System.Windows.Forms.TextBox()
+        Me.Nombre = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnCrear = New System.Windows.Forms.Button()
+        Me.txtId = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btnModificar = New System.Windows.Forms.Button()
+        Me.btnEliminar = New System.Windows.Forms.Button()
+        CType(Me.dgvEstudiantes, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvEstudiantes
+        '
+        Me.dgvEstudiantes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvEstudiantes.Location = New System.Drawing.Point(47, 321)
+        Me.dgvEstudiantes.Name = "dgvEstudiantes"
+        Me.dgvEstudiantes.RowHeadersWidth = 51
+        Me.dgvEstudiantes.RowTemplate.Height = 29
+        Me.dgvEstudiantes.Size = New System.Drawing.Size(917, 188)
+        Me.dgvEstudiantes.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(47, 288)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(87, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Estudiantes:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(169, 28)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(158, 27)
+        Me.txtNombre.TabIndex = 2
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(168, 70)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(158, 27)
+        Me.txtApellido.TabIndex = 3
+        '
+        'txtEdad
+        '
+        Me.txtEdad.Location = New System.Drawing.Point(169, 115)
+        Me.txtEdad.Name = "txtEdad"
+        Me.txtEdad.Size = New System.Drawing.Size(157, 27)
+        Me.txtEdad.TabIndex = 4
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(168, 157)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(159, 27)
+        Me.txtDireccion.TabIndex = 5
+        '
+        'txtTel
+        '
+        Me.txtTel.Location = New System.Drawing.Point(169, 203)
+        Me.txtTel.Name = "txtTel"
+        Me.txtTel.Size = New System.Drawing.Size(161, 27)
+        Me.txtTel.TabIndex = 6
+        '
+        'Nombre
+        '
+        Me.Nombre.AutoSize = True
+        Me.Nombre.Location = New System.Drawing.Point(81, 31)
+        Me.Nombre.Name = "Nombre"
+        Me.Nombre.Size = New System.Drawing.Size(64, 20)
+        Me.Nombre.TabIndex = 7
+        Me.Nombre.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 20)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Apellido"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(91, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 20)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Edad"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(78, 166)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(72, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Dirección"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(81, 207)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Telefono"
+        '
+        'btnCrear
+        '
+        Me.btnCrear.Location = New System.Drawing.Point(238, 245)
+        Me.btnCrear.Name = "btnCrear"
+        Me.btnCrear.Size = New System.Drawing.Size(92, 33)
+        Me.btnCrear.TabIndex = 12
+        Me.btnCrear.Text = "Insertar"
+        Me.btnCrear.UseVisualStyleBackColor = True
+        '
+        'txtId
+        '
+        Me.txtId.Location = New System.Drawing.Point(448, 31)
+        Me.txtId.Name = "txtId"
+        Me.txtId.Size = New System.Drawing.Size(160, 27)
+        Me.txtId.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(418, 37)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(24, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "ID"
+        '
+        'btnModificar
+        '
+        Me.btnModificar.Location = New System.Drawing.Point(448, 245)
+        Me.btnModificar.Name = "btnModificar"
+        Me.btnModificar.Size = New System.Drawing.Size(110, 33)
+        Me.btnModificar.TabIndex = 15
+        Me.btnModificar.Text = "Modificar"
+        Me.btnModificar.UseVisualStyleBackColor = True
+        '
+        'btnEliminar
+        '
+        Me.btnEliminar.Location = New System.Drawing.Point(655, 248)
+        Me.btnEliminar.Name = "btnEliminar"
+        Me.btnEliminar.Size = New System.Drawing.Size(104, 29)
+        Me.btnEliminar.TabIndex = 16
+        Me.btnEliminar.Text = "Elminar"
+        Me.btnEliminar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(996, 521)
+        Me.Controls.Add(Me.btnEliminar)
+        Me.Controls.Add(Me.btnModificar)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtId)
+        Me.Controls.Add(Me.btnCrear)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Nombre)
+        Me.Controls.Add(Me.txtTel)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.txtEdad)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgvEstudiantes)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvEstudiantes, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvEstudiantes As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents txtEdad As TextBox
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents txtTel As TextBox
+    Friend WithEvents Nombre As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnCrear As Button
+    Friend WithEvents txtId As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btnModificar As Button
+    Friend WithEvents btnEliminar As Button
+End Class
---
+++ Taller_CRUD/Form1.vb
@@ -0,0 +1,89 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Dim conn As New SqlConnection("Server=localhost\SQLEXPRESS;Database=Taller;Trusted_Connection=True;")
+
+    Private Sub Nombre_Click(sender As Object, e As EventArgs) Handles Nombre.Click
+
+    End Sub
+
+    Private Sub btnCrear_Click(sender As Object, e As EventArgs) Handles btnCrear.Click
+        Try
+            Dim command As New SqlCommand("INSERT INTO Estudiante VALUES (@nombre,@apellido,@edad,@direccion,@telefono)", conn)
+            command.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtNombre.Text
+            command.Parameters.Add("@apellido", SqlDbType.VarChar).Value = txtApellido.Text
+            command.Parameters.Add("@edad", SqlDbType.Int).Value = Convert.ToInt32(txtEdad.Text)
+            command.Parameters.Add("@direccion", SqlDbType.VarChar).Value = txtDireccion.Text
+            command.Parameters.Add("@telefono", SqlDbType.VarChar).Value = txtTel.Text
+            conn.Open()
+
+            If command.ExecuteNonQuery() = 1 Then
+                MessageBox.Show("Se registro de forma correcta el usuario")
+            Else
+                MessageBox.Show("No se logro registrar el usuario")
+            End If
+
+            conn.Close()
+            MostrarDatos()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MostrarDatos()
+    End Sub
+
+    Private Sub MostrarDatos()
+        Try
+            Dim command As New SqlCommand("SELECT * FROM Estudiante", conn)
+            Dim dataAdapter As New SqlDataAdapter(command)
+            Dim dataSet As New DataSet
+            dataAdapter.Fill(dataSet, "Estudiante")
+            dgvEstudiantes.DataSource = dataSet.Tables("Estudiante")
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnModificar_Click(sender As Object, e As EventArgs) Handles btnModificar.Click
+        Try
+            Dim command As New SqlCommand("UPDATE Estudiante SET nombre=@nombre WHERE id=@id", conn)
+            command.Parameters.Add("@nombre", SqlDbType.VarChar).Value = txtNombre.Text
+            command.Parameters.Add("@id", SqlDbType.Int).Value = Convert.ToInt32(txtId.Text)
+
+            conn.Open()
+
+            If command.ExecuteNonQuery() = 1 Then
+                MessageBox.Show("Se modifico correctamente el usuario")
+            Else
+                MessageBox.Show("No se logro modificar el usuario")
+            End If
+
+            conn.Close()
+            MostrarDatos()
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnEliminar_Click(sender As Object, e As EventArgs) Handles btnEliminar.Click
+        Dim command As New SqlCommand("DELETE FROM Estudiante WHERE id = @id", conn)
+        command.Parameters.Add("@id", SqlDbType.Int).Value = Convert.ToInt32(txtId.Text)
+        conn.Open()
+
+        If command.ExecuteNonQuery() = 1 Then
+            MessageBox.Show("Se elimino correctamente el usuario")
+        Else
+            MessageBox.Show("No se logro eliminar el usuario")
+        End If
+
+        conn.Close()
+        MostrarDatos()
+
+    End Sub
+End Class
